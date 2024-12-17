4fd964115cfa8ad0995f25e05ae5d8f8875b4009
Prantor-Hawlader <prantor502@gmail.comm>
2024-12-17 22:21:03 +0600

    Home message initialised

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ import Stepper from "@/components/ui/Stepper";
 export default function Home() {
   return (
     <div className="grid grid-cols-2 items-center place-items-center">
+      <p className="text-6xl font-mono italic text-center">
+        Web Application building in progress...
+      </p>
+
       {/* <Stepper /> */}
       {/* <LearningCard /> */}
 
