5ff3a44b70ac4ffb0472eaa81f8314d30f05d92b
Prantor-Hawlader <prantor502@gmail.com>
2025-01-04 23:33:55 +0600

    Refactor Home component: replace page.jsx with page.tsx and integrate PulseBeams component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { PulseBeams } from "@/components/SpecialBeam";
+
 export default function Home() {
   return (
     <div className="grid grid-cols-2 items-center place-items-center">
       <p className="text-6xl font-mono italic text-center">
         Web Application building in progress...
       </p>
+
+      <PulseBeams />
     </div>
   );
 }
