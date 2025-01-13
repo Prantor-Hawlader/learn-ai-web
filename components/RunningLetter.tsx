f427e70e5fccebcc56843bc4c6486bdc0408c49f
Prantor-Hawlader <prantor502@gmail.com>
2025-01-13 22:27:26 +0600

    Added Typewriter component

---
+++ components/RunningLetter.tsx
@@ -0,0 +1,25 @@
+import Typewriter from "./ui/typewriter";
+
+export default function Preview() {
+  return (
+    <div className="w-full h-full md:text-4xl lg:text-5xl sm:text-3xl text-2xl flex flex-row items-start justify-start bg-background font-normal overflow-hidden p-16 pt-48">
+      <p className="whitespace-pre-wrap">
+        <span>{"We're born 🌞 to "}</span>
+        <Typewriter
+          className="text-yellow-500"
+          cursorChar={"_"}
+          deleteSpeed={40}
+          speed={70}
+          text={[
+            "experience",
+            "dance",
+            "love",
+            "be alive",
+            "create things that make the world a better place",
+          ]}
+          waitTime={1500}
+        />
+      </p>
+    </div>
+  );
+}
