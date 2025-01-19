58e22f76df91069f9aa57e4f98381fe6fc7b7f1e
Prantor-Hawlader <prantor502@gmail.com>
2025-01-19 23:17:01 +0600

    Fix formatting in CodeBlock component: adjust indentation for code snippet

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -7,9 +7,9 @@ import { cn } from "@/lib/utils";
 
 const CodeBlock = () => {
   const myCode = `def assign_grade(score):
-if score >= 90: return "A"
-elif score >= 80: return "B"
-else: return "F"`;
+ if score >= 90: return "A"
+ elif score >= 80: return "B"
+ else: return "F"`;
 
   return (
     <div className="flex h-[140px] w-1/2 items-start justify-center overflow-hidden rounded-lg border bg-background p-2 md:shadow-xl">
